1196a9aa03e05cec71af2dce18dda4e4c02d82b6
amaxim-uf <70339344+amaxim-uf@users.noreply.github.com>
2021-01-25 08:00:25 -0500

    init commit

---
+++ HermesTranslationChat/ChatHub.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Web;
+using System.Net.Http;
+using System.Collections.Generic;
+using Microsoft.AspNet.SignalR;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace SignalRChat
+{
+    public class ChatHub : Hub
+    {
+        const string COGNITIVE_SERVICES_KEY = "5ef5adebfb0243b1ba9fec6a4b1ea3be";
+        // Endpoints for Translator Text and Bing Spell Check
+        public static readonly string TEXT_TRANSLATION_API_ENDPOINT = "https://api.cognitive.microsofttranslator.com/{0}?api-version=3.0";
+        // An array of language codes
+        private string[] languageCodes;
+
+        // Dictionary to map language codes from friendly name (sorted case-insensitively on language name)
+        private SortedDictionary<string, string> languageCodesAndTitles =
+            new SortedDictionary<string, string>(Comparer<string>.Create((a, b) => string.Compare(a, b, true)));
+
+        public void Send(string name, string language, string message)
+        {
+            // Call the addNewMessageToPage method to update clients.
+            Clients.All.addNewMessageToPage(name, language, message);
+        }
+
+        public void EchoSent(string fromCode, string toCode, string message)
+        {
+            string textToTranslate = message.Trim();
+
+            // Handle null operations: no text or same source/target languages
+            if (textToTranslate == "" || fromCode == toCode)
+            {
+                Clients.Caller.echoTranslate(message);
+                return;
+            }
+
+            // Send translation request
+            string endpoint = string.Format(TEXT_TRANSLATION_API_ENDPOINT, "translate");
+            string uri = string.Format(endpoint + "&from={0}&to={1}", fromCode, toCode);
+
+            System.Object[] body = new System.Object[] { new { Text = textToTranslate } };
+            var requestBody = JsonConvert.SerializeObject(body);
+
+            using (var client = new HttpClient())
+            using (var request = new HttpRequestMessage())
+            {
+                request.Method = HttpMethod.Post;
+                request.RequestUri = new Uri(uri);
+                request.Content = new StringContent(requestBody, Encoding.UTF8, "application/json");
+                request.Headers.Add("Ocp-Apim-Subscription-Key", COGNITIVE_SERVICES_KEY);
+                request.Headers.Add("Ocp-Apim-Subscription-Region", "eastus2");
+                request.Headers.Add("X-ClientTraceId", Guid.NewGuid().ToString());
+
+                var response = client.SendAsync(request);
+                var responseBody = response.GetAwaiter().GetResult().Content.ReadAsStringAsync();//.Content.ReadAsStringAsync();
+
+                var result = JsonConvert.DeserializeObject<List<Dictionary<string, List<Dictionary<string, string>>>>>(responseBody.GetAwaiter().GetResult());
+                var translation = result[0]["translations"][0]["text"];
+
+                string uri2 = string.Format(endpoint + "&from={0}&to={1}", toCode, fromCode);
+
+                // now translate back
+                System.Object[] body2 = new System.Object[] { new { Text = translation } };
+                var requestBody2 = JsonConvert.SerializeObject(body);
+                using (var client2 = new HttpClient())
+                using (var request2 = new HttpRequestMessage())
+                {
+                    request2.Method = HttpMethod.Post;
+                    request2.RequestUri = new Uri(uri2);
+                    request2.Content = new StringContent(requestBody2, Encoding.UTF8, "application/json");
+                    request2.Headers.Add("Ocp-Apim-Subscription-Key", COGNITIVE_SERVICES_KEY);
+                    request2.Headers.Add("Ocp-Apim-Subscription-Region", "eastus2");
+                    request2.Headers.Add("X-ClientTraceId", Guid.NewGuid().ToString());
+
+                    var response2 = client.SendAsync(request2);
+                    var responseBody2 = response2.GetAwaiter().GetResult().Content.ReadAsStringAsync();//.Content.ReadAsStringAsync();
+
+                    var result2 = JsonConvert.DeserializeObject<List<Dictionary<string, List<Dictionary<string, string>>>>>(responseBody2.GetAwaiter().GetResult());
+                    var translation2 = result[0]["translations"][0]["text"];
+
+                    // Update the translation field
+                    Clients.Caller.echoTranslate(translation2);
+                }
+            }
+        }
+
+        public void Translate(string fromCode, string toCode, string name, string message)
+        {
+            string textToTranslate = message.Trim();
+
+            // Handle null operations: no text or same source/target languages
+            if (textToTranslate == "" || fromCode == toCode)
+            {
+                Clients.Caller.postMessageTranslation(name, message);
+                return;
+            }
+
+            // Send translation request
+            string endpoint = string.Format(TEXT_TRANSLATION_API_ENDPOINT, "translate");
+            string uri = string.Format(endpoint + "&from={0}&to={1}", fromCode, toCode);
+
+            System.Object[] body = new System.Object[] { new { Text = textToTranslate } };
+            var requestBody = JsonConvert.SerializeObject(body);
+
+            using (var client = new HttpClient())
+            using (var request = new HttpRequestMessage())
+            {
+                request.Method = HttpMethod.Post;
+                request.RequestUri = new Uri(uri);
+                request.Content = new StringContent(requestBody, Encoding.UTF8, "application/json");
+                request.Headers.Add("Ocp-Apim-Subscription-Key", COGNITIVE_SERVICES_KEY);
+                request.Headers.Add("Ocp-Apim-Subscription-Region", "eastus2");
+                request.Headers.Add("X-ClientTraceId", Guid.NewGuid().ToString());
+
+                var response = client.SendAsync(request);
+                var responseBody = response.GetAwaiter().GetResult().Content.ReadAsStringAsync();//.Content.ReadAsStringAsync();
+
+                var result = JsonConvert.DeserializeObject<List<Dictionary<string, List<Dictionary<string, string>>>>>(responseBody.GetAwaiter().GetResult());
+                var translation = result[0]["translations"][0]["text"];
+
+                // Update the translation field
+                Clients.Caller.postMessageTranslation(name, translation);
+            }
+        }
+    }
+}
---
+++ HermesTranslationChat/Default.aspx.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace HermesTranslationChat
+{
+    public partial class _Default : Page
+    {
+        const string COGNITIVE_SERVICES_KEY = "5ef5adebfb0243b1ba9fec6a4b1ea3be";
+        // Endpoints for Translator Text and Bing Spell Check
+        public static readonly string TEXT_TRANSLATION_API_ENDPOINT = "https://api.cognitive.microsofttranslator.com/{0}?api-version=3.0";
+        // An array of language codes
+        private string[] languageCodes;
+
+        // Dictionary to map language codes from friendly name (sorted case-insensitively on language name)
+        private SortedDictionary<string, string> languageCodesAndTitles =
+            new SortedDictionary<string, string>(Comparer<string>.Create((a, b) => string.Compare(a, b, true)));
+
+        protected global::System.Web.UI.WebControls.DropDownList ddlLanguageSelect;
+        protected global::System.Web.UI.WebControls.Button echo;
+        protected global::System.Web.UI.WebControls.Button sendmessage;
+        protected global::System.Web.UI.WebControls.TextBox message;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            GetLanguagesForTranslate();
+
+            PopulateLanguageMenus();
+
+            UpdateScreenText();
+        }
+        
+        private void UpdateScreenText()
+        {
+            ddlLanguageSelect.Attributes["title"] = TranslateTextAsync("Select your language", "en");
+            echo.Attributes["title"] = TranslateTextAsync("Translate your message to the \nreceiver's language and then \nback to your language", "en");
+            sendmessage.Attributes["title"] = TranslateTextAsync("Translate your message to the \nreceiver's language and transmit", "en");
+            echo.Text = TranslateTextAsync("Echo", "en");
+            sendmessage.Text = TranslateTextAsync("Send", "en");
+            message.Attributes["placeholder"] = TranslateTextAsync("Type message to be translated and press Enter to send...", "en");
+            int count = echo.Text.Length;
+            echo.Width = Math.Max(count * 14 + 4, 60);
+            count = sendmessage.Text.Length;
+            sendmessage.Width = Math.Max(count * 14 + 4, 60);
+        }
+
+        private void PopulateLanguageMenus()
+        {
+            String[] userLang = Request.UserLanguages;
+  
+            // Add option to automatically detect the source language
+            int count = languageCodesAndTitles.Count;
+            foreach (string menuItem in languageCodesAndTitles.Keys)
+            {
+                ddlLanguageSelect.Items.Add(new ListItem(menuItem, languageCodesAndTitles[menuItem]));
+                if (!IsPostBack && userLang.Length > 0 && userLang[0].Substring(0, 2) == languageCodesAndTitles[menuItem])
+                {
+                    ddlLanguageSelect.SelectedValue = languageCodesAndTitles[menuItem];
+                }
+            }
+        }
+
+        private void GetLanguagesForTranslate()
+        {
+            // Send a request to get supported language codes
+            string uri = String.Format(TEXT_TRANSLATION_API_ENDPOINT, "languages") + "&scope=translation";
+            WebRequest WebRequest = WebRequest.Create(uri);
+            WebRequest.Headers.Add("Ocp-Apim-Subscription-Key", COGNITIVE_SERVICES_KEY);
+            WebRequest.Headers.Add("Accept-Language", "en");
+            WebResponse response = null;
+            // Read and parse the JSON response
+            response = WebRequest.GetResponse();
+            using (var reader = new StreamReader(response.GetResponseStream(), UnicodeEncoding.UTF8))
+            {
+                var result = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, Dictionary<string, string>>>>(reader.ReadToEnd());
+                var languages = result["translation"];
+
+                languageCodes = languages.Keys.ToArray();
+                foreach (var kv in languages)
+                {
+                    if (kv.Value["name"] == kv.Value["nativeName"])
+                    {
+                        languageCodesAndTitles.Add(kv.Value["name"], kv.Key);
+                    }
+                    else
+                    {
+                        languageCodesAndTitles.Add(kv.Value["name"] + " (" + kv.Value["nativeName"] + ')', kv.Key);
+                    }
+                }
+            }
+        }
+
+        private string TranslateTextAsync(string TextToTranslate, string fromLanguageCode)
+        {
+            string textToTranslate = TextToTranslate.Trim();
+
+            string toLanguageCode = ddlLanguageSelect.SelectedValue.ToString();
+
+            // Spell-check the source text if the source language is English
+            //if (fromLanguageCode == "en")
+            //{
+            //    if (textToTranslate.StartsWith("-"))    // don't spell check in this case
+            //        textToTranslate = textToTranslate.Substring(1);
+            //    else
+            //    {
+            //        textToTranslate = CorrectSpelling(textToTranslate);
+            //        TextToTranslate.Text = textToTranslate;     // put corrected text into input field
+            //    }
+            //}
+
+            // Handle null operations: no text or same source/target languages
+            if (textToTranslate == "" || fromLanguageCode == toLanguageCode)
+            {
+                return textToTranslate;
+            }
+
+            // Send translation request
+            string endpoint = string.Format(TEXT_TRANSLATION_API_ENDPOINT, "translate");
+            string uri = string.Format(endpoint + "&from={0}&to={1}", fromLanguageCode, toLanguageCode);
+
+            System.Object[] body = new System.Object[] { new { Text = textToTranslate } };
+            var requestBody = JsonConvert.SerializeObject(body);
+
+            using (var client = new HttpClient())
+            using (var request = new HttpRequestMessage())
+            {
+                request.Method = HttpMethod.Post;
+                request.RequestUri = new Uri(uri);
+                request.Content = new StringContent(requestBody, Encoding.UTF8, "application/json");
+                request.Headers.Add("Ocp-Apim-Subscription-Key", COGNITIVE_SERVICES_KEY);
+                request.Headers.Add("Ocp-Apim-Subscription-Region", "eastus2");
+                request.Headers.Add("X-ClientTraceId", Guid.NewGuid().ToString());
+
+                var response = client.SendAsync(request);
+                var responseBody = response.GetAwaiter().GetResult().Content.ReadAsStringAsync();//.Content.ReadAsStringAsync();
+
+                var result = JsonConvert.DeserializeObject<List<Dictionary<string, List<Dictionary<string, string>>>>>(responseBody.GetAwaiter().GetResult());
+                var translation = result[0]["translations"][0]["text"];
+
+                // Update the translation field
+                return translation;
+            }
+        }
+
+        protected void ddlLanguageSelect_Change(object sender, EventArgs e)
+        {
+            UpdateScreenText();
+        }
+
+    }
+}
